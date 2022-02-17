159ca90d19e9b6b076425549c05de076668716f1
tranquocdat69 <tranquocdataa@gmail.com>
2022-02-17 22:49:05 +0700

    first commit

---
+++ Insert_Sqlite/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Data.Sqlite;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Insert_Sqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = System.IO.File.ReadAllText("C:/Workspace/Data/data_stock_fake.cs");
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, Stock>>>>(content);
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "C:/Users/DATTQ/Downloads/sqlitestudio-3.3.3/SQLiteStudio/stock_db.db";
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                /*using (var transaction = connection.BeginTransaction())
+                {
+                    var insertCmd = connection.CreateCommand();
+
+                    foreach (var a in dictionary)
+                    {
+                        foreach (var b in dictionary[a.Key])
+                        {
+                            foreach (var c in dictionary[a.Key][b.Key])
+                            {
+                                insertCmd.CommandText = "INSERT INTO stock VALUES('" + c.Value.Code + "','" + c.Value.Price + "','" + c.Value.Quantity + "')";
+                                insertCmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
+
+                    transaction.Commit();
+                }*/
+
+                var selectCmd = connection.CreateCommand();
+                selectCmd.CommandText = "SELECT * FROM stock";
+
+                using (var reader = selectCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var code = reader.GetString(0);
+                        var price = reader.GetDouble(1);
+                        var quantity = reader.GetInt32(2);
+
+                        Console.WriteLine(code+","+price+","+quantity);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Insert_Sqlite/Stock.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insert_Sqlite
+{
+    class Stock
+    {
+        [JsonProperty("c")]
+        [DataMember(Name = "c")]
+        public string Code { get; set; }
+        [JsonProperty("p")]
+        [DataMember(Name = "p")]
+        public double Price { get; set; }
+        [JsonProperty("q")]
+        [DataMember(Name = "q")]
+        public int Quantity { get; set; }
+    }
+}
